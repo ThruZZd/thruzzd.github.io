05de0c01f8e6c4a25aa95a476c0cf7b1ef9a2190
Tobias Weyrich <tobias.weyrich@uni-konstanz.de>
2024-06-08 01:25:03 +0200

    Addad tina

---
+++ content/news/Erste-News.md
@@ -0,0 +1,6 @@
+---
+title: Erste News
+date: 08.06.2024
+---
+
+Hallo, das sind erste News
--- src/comps/Newsfeed.js
+++ src/comps/Newsfeed.js
@@ -1,9 +1,14 @@
+//import { client } from '/tina/__generated__/client'
+
 const Newsfeed = () => {
     const news = [
         {title: "Titel1", body: "Body 1", date: "07.06.2024", id:1},
         {title: "Titel2", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, facere incidunt! Non quia similique voluptates, delectus inventore voluptate, ratione, alias illo cumque modi perferendis dolores id aut sed exercitationem provident!", date: "06.06.2024", id:2},
         {title: "Titel3", body: "Body 3", date: "02.06.2024", id:3}        
     ];
+
+   
+    
     
     return (
         <div className="newsFeed">
